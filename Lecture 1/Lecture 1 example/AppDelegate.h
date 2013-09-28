51b1b193d4e65567b85036a216a3919e1a67fc68
Timothy Chong <timothychong@outlook.com>
2013-09-28 17:47:19 -0400

    upload

---
+++ Lecture 1/Lecture 1 example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lecture 1 example
+//
+//  Created by Timothy Chong on 9/28/13.
+//  Copyright (c) 2013 Timothy Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
